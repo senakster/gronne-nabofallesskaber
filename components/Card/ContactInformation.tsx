5233c4076bbe2b99d29b8669e2490f4236075041
Emil Nakayama <senakster@gmail.com>
2023-07-23 15:24:08 +0200

    added gap to address

--- components/Card/ContactInformation.tsx
+++ components/Card/ContactInformation.tsx
@@ -6,9 +6,9 @@ const ContactInformation = (contact: TContact) => {
     return (
         <>
             <ul className="flex flex-col gap-2 my-auto">
-                <li className={`flex flex-wrap`}><span>Adresse: </span><span>{address}</span> <span>{postal}</span> <span>{city}</span></li>
-                <li className={`flex flex-wrap`}><span>Tlf.: </span><span><a href={`tel:${phone}`}>{phone}</a></span></li>
-                <li className={`flex flex-wrap`}><span>Email: </span><span><a href={`mailto:${email}`}>{email}</a></span></li>
+                <li className={`flex flex-wrap gap-1`}><span>Adresse: </span><span>{address}</span> <span>{postal}</span> <span>{city}</span></li>
+                <li className={`flex flex-wrap gap-1`}><span>Tlf.: </span><span><a href={`tel:${phone}`}>{phone}</a></span></li>
+                <li className={`flex flex-wrap gap-1`}><span>Email: </span><span><a href={`mailto:${email}`}>{email}</a></span></li>
             </ul>
         </>
     )
