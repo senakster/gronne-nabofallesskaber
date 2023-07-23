fc6bba28bca658cfd42340954c2a8658cbaaf706
Emil Nakayama <senakster@gmail.com>
2023-07-23 15:19:09 +0200

    remove pages  folder

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,18 +1,9 @@
-import { useNextSanityImage } from 'next-sanity-image';
 import { client }  from '@/sanity/lib/client'
-import { TPortrait } from '@/types/Person';
-import { SanityAsset, SanityImageSource } from '@sanity/image-url/lib/types/types';
+import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import imageUrlBuilder from '@sanity/image-url'
-import { SanityReference } from 'next-sanity';
-
-export const getSanityImgProps = (image: SanityImageSource) => {
-    return useNextSanityImage(client, image);
-}
 
 const builder = imageUrlBuilder(client)
 
 export function urlFor(image: SanityImageSource) {
     return builder.image(image)
 }
-
-// export default getSanityImgProps
--- pages/frontpage.tsx
+++
@@ -1,8 +0,0 @@
-const Frontpage = () => {
-    return (
-        <>
-        </>
-    )
-}
-
-export default Frontpage
